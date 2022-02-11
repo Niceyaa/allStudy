f74e1b5ccf3f63e700d3134f33c6f4de58b7d765
niceYaa <1643391875@qq.com>
2022-02-11 10:02:28 +0800

    整理

--- work/test.js
+++ work/test.js
@@ -34,31 +34,51 @@ With sixty seconds’ worth of distance run,
 Yours is the Earth and everything that’s in it,   
 And — which is more — you’ll be a Man, my son!`
 
-letterStr = ClearBr(letterStr)
-let letterArr = letterStr.split(' ') // 以空格分隔
-letterArr = letterArr.filter(item => item.length !== 0) // 清除空字符串
-letterArr = [...new Set(letterArr)] // 去重
+// 获取weight最重的三个word
+function getTopThree(str) {
+    if (!str) {
+        return
+    }
+    str = ClearBr(str)
+    let letterArr = str.split(' ') // 以空格分隔
+    letterArr = letterArr.filter(item => item.length !== 0) // 清除空字符串
+    letterArr = [...new Set(letterArr)] // 去重
 
-let obj = letterArr.reduce((prev,cur)=>{
-    let lowerStr = cur.toLowerCase(cur)
-    let weight = 0
-    for(let i = 0;i<lowerStr.length;i++){
-        if(lowerStr[i]>='a'&&lowerStr[i]<='z'){
-            weight+=lowerStr[i].charCodeAt()-97 // a 的code为97
+    let obj = letterArr.reduce((prev, cur) => {
+        let lowerStr = cur.toLowerCase(cur)
+        let weight = 0
+        for (let i = 0; i < lowerStr.length; i++) {
+            if (lowerStr[i] >= 'a' && lowerStr[i] <= 'z') {
+                weight += lowerStr[i].charCodeAt() - 97 // a 的code为97
+            }
+        }
+        prev[cur] = weight
+        return prev
+    }, {})
+
+    let objArr = Object.entries(obj)
+    objArr.sort(compareFn) // 根据weight从高到底排序
+
+    let topRes = []
+    let topWeight = []
+    for (let i = 0; i < objArr.length; i++) {
+        topRes.push(objArr[i][0])
+        if (!topWeight.includes(objArr[i][1])) {
+            topWeight.push(objArr[i][1])
+        }
+        if (topWeight.length === 3) {
+            break
         }
     }
-    prev[cur] = weight
-    return prev
-},{})
+    return topRes
+}
 
-let valueArr = [...new Set(Object.values(obj).sort((a,b)=>b-a))] // 将所有的weight都存到数组里面，为了前三的值有重复的去一下重
+console.log('top3', getTopThree(letterStr))
 
-for(let i = 0;i<valueArr.length)
-
-console.log('letterArr------', letterArr)
-console.log('obj------', obj)
-console.log('valueArr------', valueArr)
-console.log('entriesArr------', Object.entries(obj))
+// 排序函数
+function compareFn(a, b) {
+    return a[1] > b[1] ? -1 : 1
+}
 // 去除 \n 和 ，
 function ClearBr(key) {
     key = key.replace(/<\/?.+?>/g, "");
