659e15bbbd14adaaa471bfb085b1e07b251d578c
niceYaa <1643391875@qq.com>
2022-03-02 17:15:50 +0800

    zhengli

--- work/promise.js
+++ work/promise.js
@@ -9,7 +9,17 @@
         setTimeout(() => {
             resolve(p1)
             console.log('p2')
-        }, 1000);
+        }, 3000);
     })
     p2.then(res => console.log(res)).catch(err => console.log(err))
+
+    const p3 = new Promise((resolve,reject)=>{
+        console.log('p3')
+        return 'pppppp'
+    })
+    p3.then(res=>{
+        console.log('没有resolve我会执行吗')
+    }).catch(err=>{
+        console.log('p3 Error',err)
+    })
 })()
