ca85cfe939f90f4aae01ed0235e7a427e72e52ba
liangchaogui <liangchaogui@Fumaai.com>
2022-10-14 17:37:03 +0800

    '复习promise'

--- work/call、bind、apply.js
+++ work/call、bind、apply.js
@@ -1,18 +1,53 @@
-var name = '小王',age = 18
+var name = '小王', age = 18
 var obj = {
-    name:'小张',
-    objAge:this.age,
-    myFun:function(fm,t){
+    name: '小张',
+    objAge: this.age,
+    myFun: function (fm, t) {
         console.log(`${this.name}年龄${this.age}来自${fm}去往${t}`)
     }
 }
 var dm = {
-    name:'德玛',
-    age:20
+    name: '德玛',
+    age: 20
 }
-console.log('objAge',obj.objAge)
+console.log('objAge', obj.objAge)
 // obj.myFun()
 
-obj.myFun.call(dm,'成都','上海')
-obj.myFun.apply(dm,['成都','上海'])
-obj.myFun.bind(dm,'成都','上海')()
+// obj.myFun.call(dm,'成都','上海')
+// obj.myFun.apply(dm,['成都','上海'])
+// obj.myFun.bind(dm,'成都','上海')()
+
+// 自己封装方法
+Function.prototype.myApply = function (context = window) {
+    context.fn = this
+    let res
+    if (arguments[1]) {
+        res = context.fn(...arguments[1])
+    } else {
+        res = context.fn()
+    }
+    console.log('context', arguments)
+    delete context.fn
+    console.log('context', context)
+    return res
+}
+obj.myFun.myApply(dm, ['cd', 'ssh'])
+
+Function.prototype.myCall = function (context = window) {
+    context.fn = this
+    let args = [...arguments].slice(1)
+    return context.fn(...args)
+}
+
+obj.myFun.myCall(dm, 'cd1', 'ssh1')
+
+// obj.myFun.bind(dm,'成都','上海')()
+Function.prototype.myBind = function (context = window) {
+    context.fn = this
+    let curArgs = [...arguments].slice(1)
+    let dd = function(){
+        context.fn(...curArgs)
+    }
+    return dd
+}
+obj.myFun.myBind(dm,'bind成都','bind上海')()
--- work/promise面试题.js
+++ work/promise面试题.js
@@ -1,4 +1,4 @@
-/* (async() => {
+ (async() => {
    console.log(1)
    await new Promise((resolve,reject)=>{
        console.log(2)
@@ -6,16 +6,20 @@
            console.log(3)
        }, 3000);
     //    reject()
-    //    resolve()
-       return 11
+       resolve()
+       // return 11
        console.log(4)
    }).then(res=>{
        console.log(5)
    })
    console.log(6)
-})() */
-/* process.nextTick(()=>{console.log(1)})
-setImmediate(()=>{
+})()
+//  1 2 4 6 5 3
+
+/*process.nextTick(() => {
+    console.log(1)
+})
+setImmediate(() => {
     console.log(2)
 })
 console.log(3)
@@ -28,38 +32,43 @@ setTimeout(() => {
 setTimeout(() => {
     console.log(6)
 }, 10);
-console.log(7) */
+console.log(7)*/
+// 3 7 1 2 4 6 5
 
-/* let a;
-const b = new Promise((resolve, reject) => {
-  console.log('promise1');
-  resolve();
-console.log('b',b)
+/*let a;
+let b = new Promise((resolve, reject) => {
+    console.log('promise1');
+    resolve();
+    console.log('b')
+    // console.log('b', b)
 }).then(() => {
-  console.log('promise2');
+    console.log('promise2');
 }).then(() => {
-  console.log('promise3');
+    console.log('promise3');
 }).then(() => {
-  console.log('promise4');
-  return 'hhhh'
+    console.log('promise4');
+    return 'hhhh'
 });
 
 
-a = new Promise(async (resolve, reject) => {
-  console.log(a);
-  await b;
-  console.log(a);
-  console.log('after1');
-  await a
-  resolve(true);
-  console.log('after2');
+a = new Promise( async(resolve, reject) => {
+    console.log('a1',a);
+    await b;
+    console.log('a2',a);
+    console.log('after1');
+    await a
+    resolve(true);
+    console.log('after2');
 });
 
-console.log('end'); */
-const p = new Promise((resolve,reject)=>{
+console.log('end');*/
+
+// promise1 b  undefined end promise2 promise3 promise4 a2 after1
+
+/*const p = new Promise((resolve,reject)=>{
     console.log(1)
     resolve(11)
     console.log(2)
 }).then(2).then(res=>{
     console.log(4,res)
-})
+})*/
